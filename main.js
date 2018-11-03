c754569b1a45bab6bb8542fb0b6e0941dcf6f262
Claudiu Nicola <claudiunicola96@gmail.com>
2018-11-03 20:39:06 +0200

    roby the master of html5

---
+++ main.js
@@ -0,0 +1,43 @@
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext ('2d');
+
+var radius = 10;
+var dragging = false;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+context.lineWidth = radius*2;
+ 
+var putPoint = function(e){
+	if(dragging){
+		context.lineTo(e.clientX, e.clientY);
+		//context.lineTo(e.offsetX, e.offsetY);
+		context.stroke();
+		context.beginPath();
+		context.arc(e.clientX, e.clientY, radius, 0, Math.PI*2);
+		//context.arc(e.offsetX, e.offsetY, radius, 0, Math.PI*2);
+		context.fill();
+		context.beginPath();
+		context.moveTo(e.clientX, e.clientY);
+		//context.moveTo(e.offsetX, e.offsetY);
+	}
+}
+
+var engage = function(e){
+	dragging = true;
+	putPoint(e);
+}
+
+var disengage = function(){
+	dragging = false;
+	context.beginPath();
+}
+
+canvas.addEventListener('mousedown', engage);
+canvas.addEventListener('mousemove', putPoint);
+canvas.addEventListener('mouseup', disengage);
+canvas.addEventListener ('mouseout',disengage);
+
+
+   

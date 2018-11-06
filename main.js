fde5659650a372f7c9ecc8e4474155d082f2cf32
Robert Reut <reutrobert96@yahoo.com>
2018-11-07 00:31:25 +0200

    mobile fix

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ canvas.height = window.innerHeight;
 context.lineWidth = radius*2;
  
 var putPoint = function(e){
-	event.preventDefault(); 
+	e.preventDefault(); 
 	if(dragging){
 		context.lineTo(e.clientX, e.clientY);
 		//context.lineTo(e.offsetX, e.offsetY);

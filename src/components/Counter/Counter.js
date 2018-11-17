e05d8b685638468f63391238fbdaf61436427bae
Marina Popa <marina.popa@lola.tech>
2018-11-17 16:13:43 +0200

    refactor compliance modal

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -3,6 +3,8 @@ import Button from '../Button';
 import SettingsModal from '../SettingsModal';
 import 'react-circular-progressbar/dist/styles.css';
 import SegmentedProgressbar from './SegmentedProgressbar';
+import './Counter.css';
+
 const percentage = 66;
 
 export default class Counter extends React.Component {
@@ -25,16 +27,16 @@ export default class Counter extends React.Component {
 
   render() {
     return(
-      <React.Fragment>
-        <div className = 'Counter' style={{height:300 , width:300,}}>
-			<SegmentedProgressbar 
-				percentage={percentage}
-			/>
-		</div>
-		<a href={window.drawingUrl} className="button" download="My-Romania.png">DESCARCĂ</a>
+      <div className="container">
+        <div className="counter" style={{height:300 , width:300,}}>
+          <SegmentedProgressbar
+            percentage={percentage}
+          />
+        </div>
+        <a href={window.drawingUrl} className="button" download="My-Romania.png">DESCARCĂ</a>
         <Button handleClick={this.toggleModal} buttonText="SETARI"/>
         <SettingsModal open={this.state.modalOpen} closeModal={this.toggleModal}/>
-      </React.Fragment>
+      </div>
     );
   }
 }

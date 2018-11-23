a2b3948e782a36d04a30233988a939b58da55614
alex-bajzat <alex.bajzat@gmail.com>
2018-11-23 16:24:33 +0200

    added intro logo

--- src/components/IntroPage/IntroPage.js
+++ src/components/IntroPage/IntroPage.js
@@ -25,12 +25,13 @@ export default class IntroPage extends React.Component {
 
             <React.Fragment>
                 <div className="container" ref={this.toRef}>
-                    <div>
+                    <div class="logo"></div>
+                    <div class="logo-intro">
                         <div class="inline"><div className="checked"></div><div class="text">Inscrie-te</div> </div>
                         <div className="rect"></div>
-                        <div class="inline"><div className="pencil"></div> <div class="text">Inscrie-te</div> </div>
+                        <div class="inline"><div className="pencil"></div> <div class="text">Creioneaza-ti gandul</div> </div>
                         <div className="rect"></div>
-                        <div class="inline"><div className="romania"></div> <div class="text">Inscrie-te</div></div>
+                        <div class="inline"><div className="romania"></div> <div class="text">Fii parte din cel mai mare mozaic virtual</div></div>
                     </div>
                     <Button handleClick={this.participate} buttonText="TRIMITE UN GÂND BUN" />
                 </div>

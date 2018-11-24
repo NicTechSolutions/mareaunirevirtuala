5b8574027e4f350961af0f272397ea281d43e9f5
Claudiu Nicola <claudiunicola96@gmail.com>
2018-11-24 17:04:58 +0200

    show counter

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import SettingsModal from '../SettingsModal';
 import 'react-circular-progressbar/dist/styles.css';
 import SegmentedProgressbar from './SegmentedProgressbar';
@@ -9,8 +10,9 @@ import downloadIcon from './assets/download1.svg'
 import settingsIcon from './assets/settings.svg';
 import drawIcon from './assets/tools.svg';
 import Button from '../Button';
+import Constants from '../../constants/Constants';
 
-const number = 66;
+// const number = 66;
 
 const milestones = [100, 1000, 10000, 100000, 1000000];
 
@@ -24,7 +26,14 @@ class Counter extends React.Component {
 
     this.state = {
       modalOpen: false,
+      number: 0
     }
+    axios.get(`${Constants.API_URL}/users/counter`)
+    .then((response) => {
+      this.setState({ number:response.data} );
+    }).catch((err) => {
+      console.log(err);
+    });
   }
 
   toggleModal() {
@@ -47,11 +56,11 @@ class Counter extends React.Component {
         <div className="settingsButton" onClick={this.toggleModal}></div>
         <div className="counter" style={{ height: 200, width: 200, marginBottom: 20 }}>
           <SegmentedProgressbar
-            percentage={(number / (milestones.find(m => m > number))) * 100}
-            text={number}
+            percentage={(this.state.number / (milestones.find(m => m > this.state.number))) * 100}
+            text={this.state.number}
           />
         </div>
-        <p className="submit-text">Pana acum, {number} de romani au desenat Romania asa cum si-au dorit!</p>
+        <p className="submit-text">Pana acum, {this.state.number} de romani au desenat Romania asa cum si-au dorit!</p>
         {
           window.drawingUrl ?
             <Button handleClick={this.downloadImage} buttonText="Descarca"></Button>

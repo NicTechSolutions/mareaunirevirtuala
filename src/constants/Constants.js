eeebf3409151848eef684ac0bc73e46e47734a8b
alex-bajzat <alex.bajzat@gmail.com>
2018-11-29 19:27:53 +0200

    deploy config

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,6 +1,6 @@
 
 export default {
-    API_URL :"http://localhost:4000/api",
+    API_URL :"https:/mareaunirevirtuala.ro/api",
     RECAPTCHA_KEY: "6Lf4330UAAAAAKZF_OEM-YhbNB9Mqjq6uGVtJkhE",
     TEST_RECAPTCHA_KEY: "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
 };

5c0d935056bfd4625c91d96d69f63cde3f0fb211
Claudiu Nicola <claudiunicola96@gmail.com>
2018-11-30 14:25:37 +0200

    graba strica treaba

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,6 +1,6 @@
 
 export default {
-    API_URL :"http://localhost:4000/api",
+    API_URL :"https://www.mareaunirevirtuala.ro/api",
     RECAPTCHA_KEY: "6LfdBH4UAAAAAOj5nwbmhX6d65Jacq5UNblbz7tM",
     TEST_RECAPTCHA_KEY: "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
 };
